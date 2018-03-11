2c32e301fd5122df3ce05d95d77a9cedf3a71741
bandit197950 <ahtoxa1979@yahoo.com>
2018-03-11 23:00:36 +0300

    Update user.php
    1) Исправлено форматирование
    2) Исправлена опечатка в сообщении о том, что списываемая сумма больше баланса.

--- www/model/user.php
+++ www/model/user.php
@@ -13,25 +13,25 @@
 		public function WriteOffAmount($id, $amount)
 		{
 		    $ok = true;
-            if(($user = $this->GetUserInfo($id))) {
-                $user['balance'] -= round($amount, 2);
-                if($user['balance'] >= 0) {
-                    $r = $this->db->QueryUpdate("users", $user, "`id`=" . (int)$id);
-                    $ok = !!$r;
-                    if(!ok) {
-                        $this->Error = $this->db->GetLastError();
-                    }
-                }
-                else {
-                    $this->Error = "Can't write off amount, because it's greater when balance";
-                    $ok = false;
-                }
-            }
-            else {
-                $this->Error = "User not found (id=$id)";
-                $ok = false;
-            }
-			return $ok;
+		    if(($user = $this->GetUserInfo($id))) {
+			$user['balance'] -= round($amount, 2);
+			if($user['balance'] >= 0) {
+			    $r = $this->db->QueryUpdate("users", $user, "`id`=" . (int)$id);
+			    $ok = !!$r;
+			    if(!ok) {
+				$this->Error = $this->db->GetLastError();
+			    }
+			}
+			else {
+			    $this->Error = "Can't write off amount, because it's greater whan balance";
+			    $ok = false;
+			}
+		    }
+		    else {
+			$this->Error = "User not found (id=$id)";
+			$ok = false;
+		    }
+		    return $ok;
 		}
 		
 	};
